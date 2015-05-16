2e3d7ac4135c3a2dc17d8db2366a49def2486dcf
cuong minh <cuong@techmaster.vn>
2015-05-16 23:50:33 +0700

    string_method

--- string_method.js
+++ string_method.js
@@ -6,5 +6,12 @@ var quotes = "Darkness cannot drive out darkness; " +
     "Hate cannot drive out hate; " +
     "only love can do that.";
 console.log(quotes);
-var pos = quotes.lastIndexOf("darkness");
-console.log("pos = ", pos);
+var firstPost = quotes.indexOf("only");
+var lastPos = quotes.lastIndexOf("only");
+console.log("first pos = ", firstPost, ". last post = ", lastPos);
+
+//Tại sao không vào đây xem hết các ví dụ?
+//http://www.w3schools.com/js/js_string_methods.asp
+
+var drivePos = quotes.search("ca..ot");  //search -> regular expression
+console.log(drivePos);
