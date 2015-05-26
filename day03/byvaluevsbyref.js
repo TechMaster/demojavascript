afc25baafc923c3b231f8c27db98ddd4ed244588
Trinh Minh Cuong <cuong@techmaster.vn>
2015-05-26 19:33:58 +0700

    Update for Day 03

--- day03/byvaluevsbyref.js
+++ day03/byvaluevsbyref.js
@@ -7,9 +7,26 @@ var a = 1,
 function makeChange(_a, _b) {
     _a = 2;
     console.log(_a);
-    arguments[1] = { foo: 'baz' };
+    //arguments[1] = { foo: 'Fun qua' };
+    _b.foo = 'Fun qua';
 }
 makeChange(a, b);
 
 console.log(a === a_); //=> true
 console.log(b === b_); //=> true
+console.log(b);
+
+function Car()
+{
+    this.value = 5;
+}
+
+function changeCar(obj) {
+    obj.value = 100;
+}
+
+//var obj = new Car();
+var obj = {value : 5};
+console.log(obj.value);
+changeCar(obj);
+console.log(obj.value);
